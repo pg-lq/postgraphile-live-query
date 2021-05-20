e16107dc2571da08cd49b8b50ca78f3dad9196c1
Venryx <venryx@gmail.com>
2021-05-20 07:45:28 -0700

    * Fixed package.json in the subpackages to allow publishing.

--- package-scripts.js
+++ package-scripts.js
@@ -1,14 +1,25 @@
 const scripts = {};
 module.exports.scripts = scripts;
 
+const goPG = "cd Packages/postgraphile-plugin";
+const goAP = "cd Packages/apollo-plugin";
+
 Object.assign(scripts, {
 	"pg-plugin": {
-		dev: "cd Packages/postgraphile-plugin && snowpack build --watch",
-		devTS: "cd Packages/postgraphile-plugin && tsc --watch --declaration --emitDeclarationOnly --declarationDir Build/Types",
+		dev: `${goPG} && snowpack build --watch`,
+		devTS: `${goPG} && tsc --watch --declaration --emitDeclarationOnly --declarationDir Build/Types`,
+		pub: {
+			same: `${goPG} && npm publish && git push --follow-tags`,
+			patch: `${goPG} && npm version patch && npm publish && git push --follow-tags`
+		}
 	},
 	"ap-plugin": {
-		dev: "cd Packages/apollo-plugin && snowpack build --watch",
-		devTS: "cd Packages/apollo-plugin && tsc --watch --declaration --emitDeclarationOnly --declarationDir Build/Types",
+		dev: `${goAP} && snowpack build --watch`,
+		devTS: `${goAP} && tsc --watch --declaration --emitDeclarationOnly --declarationDir Build/Types`,
+		pub: {
+			same: `${goAP} && npm publish && git push --follow-tags`,
+			patch: `${goAP} && npm version patch && npm publish && git push --follow-tags`
+		}
 	},
 	dev: `concurrently --kill-others --names pg,pgt,ap,apt "npm start pg-plugin.dev" "npm start pg-plugin.devTS" "npm start ap-plugin.dev" "npm start ap-plugin.devTS"`,
 });
