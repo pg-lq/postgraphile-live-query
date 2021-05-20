a074fcf3c79eb93f58ca8b74f5ed7f23e21e910a
Venryx <venryx@gmail.com>
2021-05-20 07:58:11 -0700

    * Removed the " && git push --follow-tags" part from the publish scripts. (sometimes the version may be incremented for only some of the plugins, so it's confusing if the monorepo's git tags have version numbers that only apply to a subset of the plugins)

--- package-scripts.js
+++ package-scripts.js
@@ -9,16 +9,16 @@ Object.assign(scripts, {
 		dev: `${goPG} && snowpack build --watch`,
 		devTS: `${goPG} && tsc --watch --declaration --emitDeclarationOnly --declarationDir Build/Types`,
 		pub: {
-			same: `${goPG} && npm publish && git push --follow-tags`,
-			patch: `${goPG} && npm version patch && npm publish && git push --follow-tags`
+			same: `${goPG} && npm publish`,
+			patch: `${goPG} && npm version patch && npm publish`
 		}
 	},
 	"ap-plugin": {
 		dev: `${goAP} && snowpack build --watch`,
 		devTS: `${goAP} && tsc --watch --declaration --emitDeclarationOnly --declarationDir Build/Types`,
 		pub: {
-			same: `${goAP} && npm publish && git push --follow-tags`,
-			patch: `${goAP} && npm version patch && npm publish && git push --follow-tags`
+			same: `${goAP} && npm publish`,
+			patch: `${goAP} && npm version patch && npm publish`
 		}
 	},
 	dev: `concurrently --kill-others --names pg,pgt,ap,apt "npm start pg-plugin.dev" "npm start pg-plugin.devTS" "npm start ap-plugin.dev" "npm start ap-plugin.devTS"`,
